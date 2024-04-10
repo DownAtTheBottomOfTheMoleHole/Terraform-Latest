ad84b76f99ac4739191127b25dc44e72602d6669
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2024-04-10 14:51:53 +0100

    Update Terraform version to 1.7.5 in chocolateyInstall.ps1

--- packages/scripts/chocolateyInstall.ps1
+++ packages/scripts/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url = 'https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_windows_386.zip'
-$url64 = 'https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_windows_amd64.zip'
-$checksum = 'f19e749bff7c5a44c07aa8ec7efe8e3986105691b92df93f62de22efb523a983'
-$checksum64 = '5c4644f8c83f3851b70e3c3a52c68e36949b1f8436617577a69af162b4a3c815'
+$url = 'https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_windows_amd64.zip'
+$checksum = '2639c9444c6091fd5ad76f112040d592e99931489582ada4d485c12a64a79052'
+$checksum64 = '9b7be6ae159191ec1f4b5b9d27529ae5243e41020fb545c0041235bec8d92269'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
